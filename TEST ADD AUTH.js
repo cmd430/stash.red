61728deaea94e5866eeda3b31c3c8afbc7ccb338
Bradley Treweek <cmd430@gmail.com>
2018-11-21 22:17:35 +1300

    test adding new auth keys

---
+++ TEST ADD AUTH.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+
+const config = require('./config.js')
+const app = {
+  db: mongoose
+}
+
+if (config.mongo.auth.enabled) {
+  app.db.connect(`mongodb://${config.mongo.user}:${config.mongo.pass}@${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  })
+} else {
+  app.db.connect(`mongodb://${config.mongo.host}:${config.mongo.port}/${config.mongo.db}`, {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  })
+}
+
+const models = require('./routes/models.js')(app)
+
+new models.auth({
+  id: crypto.randomBytes(10).toString('hex')
+})
+.save((err, product) => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(product)
+  }
+})
+
+models.auth.aggregate([{
+  $project: {
+    _id: 0,
+    id: 1
+  }
+}])
+.exec()
+.then(docs => {
+  console.log(docs)
+})
+.catch(err => {
+  console.error(err)
+})
+
+
