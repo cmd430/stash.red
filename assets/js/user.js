9767f902b6bfa9e9b72c26c23b8fbdd191263783
Bradley Treweek <cmd430@gmail.com>
2018-12-08 16:48:16 +1300

    user page now shows as a grid

---
+++ assets/js/user.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  for (let i = 1; i < 5; i++) {
+    // Add 4 spacers to make the grid (its flex)
+    // look nice on non full rows
+    let spacer = document.createElement('div')
+    spacer.classList.add('spacer')
+    document.querySelector('#container').appendChild(spacer)
+  }
+})
