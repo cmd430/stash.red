7443a3d7b24298dc2557054dce0d63ac9e744f5d
Bradley Treweek <cmd430@gmail.com>
2018-12-12 21:54:04 +1300

    User page grid thumbnails are now not stretched

--- assets/js/user.js
+++ assets/js/user.js
@@ -9,8 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.preview').forEach(preview => {
     // any missing thumbnails will be
     // changed to the generic icon
-    if (preview.getAttribute('style') === 'background-image: url(\'\')') {
-      preview.setAttribute('style', `background-image: url('${preview.dataset.fallback}')`)
+    if (preview.getAttribute('style') === 'background: url(\'\') center center / contain no-repeat') {
+      preview.setAttribute('style', `background: url('${preview.dataset.fallback}') center center / contain no-repeat`)
     }
+    // Add transperancy background
+    preview.setAttribute('style', `${preview.getAttribute('style')}, linear-gradient(45deg, rgb(221, 221, 221) 25%, rgba(0, 0, 0, 0) 25%) 0 0/20px 20px, linear-gradient(-45deg, rgb(221, 221, 221) 25%, rgba(0, 0, 0, 0) 25%) 0 10px/20px 20px, linear-gradient(45deg, rgba(0, 0, 0, 0) 75%, rgb(221, 221, 221) 75%) 10px -10px/20px 20px, linear-gradient(-45deg, rgba(0, 0, 0, 0) 75%, rgb(221, 221, 221) 75%) -10px 0px/20px 20px, rgb(255, 255, 255)`)
   })
 })
