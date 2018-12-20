4a7623b7b625e97ef865c6aa448f70152064927e
Bradley Treweek <cmd430@gmail.com>
2018-12-20 21:30:14 +1300

    remove embed (didnt work)

--- logic/common/formatResults.js
+++ logic/common/formatResults.js
@@ -22,9 +22,6 @@ module.exports = (config, app, common) => {
         result.path = `${req.protocol}://${req.hostname}${result.path}`
         result.directpath = `${req.protocol}://${subdomain}.${req.hostname}/${result.meta.filename}`
         result.downloadpath = `${req.protocol}://${app.subdomain.download.name}.${req.hostname}/${type}/${result.meta.filename}`
-        if (type === 'audio' || type === 'video') {
-          result.embedpath = `${result.path.replace('/f/', '/e/')}`
-        }
       }
     }
     let format = result => {
