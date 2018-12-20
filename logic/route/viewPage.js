79fe17bd4959fed350daacc8ff489092981359c8
Bradley Treweek <cmd430@gmail.com>
2018-12-20 22:16:32 +1300

    gave up making audio work in discord :/

--- logic/route/viewPage.js
+++ logic/route/viewPage.js
@@ -14,7 +14,7 @@ module.exports = (config, app, common, route) => {
       // Serve our Homepage
       return res.render('index.hbs', dynamic)
     } else {
-      let typeLong = (type === 'f' ? 'file' : (type === 'a' ? 'album' : (type === 'u' ? 'user' : 'embed')))
+      let typeLong = (type === 'f' ? 'file' : (type === 'a' ? 'album' : 'user'))
       let id = req.params.id
       let rawJSON = false
       if (req.params.id.includes('.json')) {
@@ -22,7 +22,6 @@ module.exports = (config, app, common, route) => {
         rawJSON = true
       }
       switch (type) {
-        case 'e':
         case 'f': // File
           return common.getDBFile(id, render)
         case 'a': // Album
