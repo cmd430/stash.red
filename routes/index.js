19fcd87739cfdee9f6cad3d9c1f009c0e947e3a8
Bradley Treweek <cmd430@gmail.com>
2019-07-30 22:29:00 +1200

    add some route for home

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
+import createError from 'http-errors'
 
 export default Router()
-.get('/', (req, res, next) => res.render('index', { // GET home page.
-  title_fragment: 'home'
-}))
+  .get('/', (req, res, next) => res.render('index', { title_fragment: 'home' })) // homepage.
+  .post('/', (req, res, next) => res.sendStatus(200))                            // handle uploads from homepage.
+  .all('/', (req, res, next) => next(createError(501)))                          // handle unused methods
