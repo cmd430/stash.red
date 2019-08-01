48aa308ae9ec4945017e6ecc715c97c5cbd67d22
Bradley Treweek <cmd430@gmail.com>
2019-08-01 19:51:57 +1200

    serve direct links

--- routes/direct.js
+++ routes/direct.js
@@ -1,6 +1,7 @@
-import { Router } from 'express'
+
+import { join } from 'path'
+import express, { Router } from 'express'
 import createError from 'http-errors'
-import { debug } from '../utils/logger'
 
 /*
  *  Direct Links
@@ -8,33 +9,13 @@ import { debug } from '../utils/logger'
 
 export default Router()
 
+  // Server Static
+  .use(express.static(join(__dirname,'..', 'storage', 'image')))
+  .use(express.static(join(__dirname,'..', 'storage', 'audio')))
+  .use(express.static(join(__dirname,'..', 'storage', 'video')))
+
   // GET Method Routes
-  .get('/', (req, res, next) => res.redirect(`${req.protocol}://${req.hostname.split(`${req.subdomains[0]}.`)[1]}`))
-  .get('/:file', (req, res, next) => {
-
-    debug(req.params.file)
-    debug(req.subdomains.toString())
-
-    let title_fragment
-    switch (req.subdomains.toString()) {
-      case config.server.subdomains.image:
-        title_fragment = 'image'
-        break
-      case config.server.subdomains.audio:
-        title_fragment = 'audio'
-        break
-      case config.server.subdomains.video:
-        title_fragment = 'video'
-        break
-      default:
-        title_fragment = 'direct'
-    }
-    res.render('debug', {
-      title_fragment: title_fragment,
-      route: `${req.baseUrl}${req.path}`
-    })
-  })
+  .get('/', (req, res, next) => res.redirect(`${req.protocol}://${req.hostname.replace('direct.', '')}`))
 
   // Method Not Implimented
   .all('/', (req, res, next) => next(createError(501)))
-  .all('/:file', (req, res, next) => next(createError(501)))
