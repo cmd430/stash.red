2a44e158bc17c777906439b11fb23aeefe6cc12b
Bradley Treweek <cmd430@gmail.com>
2019-08-18 23:53:13 +1200

    fix misspelling of thumbnail

--- utils/uploader.js
+++ utils/uploader.js
@@ -546,13 +546,13 @@ async function createThumbnail (fileinfo, req) {
       case 'text':
         temp_thumbnail = await new Promise((resolve, reject) => {
           let input = fileinfo.path.replace(/\\/g, '/').replace(':', '\\:')
-          let font = join('..', 'public', 'font', 'thumbnial.ttf').replace(/\\/g, '/').replace(':', '\\:')
+          let font = join(__dirname, '..', 'public', 'font', 'thumbnail.ttf').replace(/\\/g, '/').replace(':', '\\:')
 
           simpleThumbnail('', null, null, {
             args: [
               '-f lavfi',
               `-i color=c=white:s=640x480:d=5.396`,
-              `-filter_complex "drawtext=textfile='${input}':x=0:y=0:font=consolas:fontfile='${font}':fontsize=13:fontcolor=000000"`
+              `-filter_complex "drawtext=textfile='${input}':x=0:y=0:fontfile='${font}':fontsize=13:fontcolor=000000"`
             ],
             path: ffmpeg.path
           })
