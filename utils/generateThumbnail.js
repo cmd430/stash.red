690c22c867ca1ff4f4c8cd5ccc48bd110d2ee762
cmd430 <cmd430@gmail.com>
2023-10-20 21:09:20 +1300

    thumbnail generation for audio (with embedded cover) and video files

--- utils/generateThumbnail.js
+++ utils/generateThumbnail.js
@@ -1,11 +1,99 @@
+import { spawn } from 'node:child_process'
+import { Readable } from 'node:stream'
+import ffmpegBin from 'ffmpeg-static'
 import sharp from 'sharp'
 
-// TODO: Video Thumbnail Generation
+async function ffmpeg (inputBuffer, type) {
+  const args = {
+    'video': [
+      '-r',
+      '1',
+      '-i',
+      'pipe:0',
+      '-vframes',
+      '1',
+      '-f',
+      'image2',
+      '-q:v',
+      '1',
+      '-c:v',
+      'mjpeg',
+      'pipe:1'
+    ],
+    'audio': [
+      '-i',
+      'pipe:0',
+      '-f',
+      'image2',
+      '-q:v',
+      '1',
+      '-c:v',
+      'mjpeg',
+      'pipe:1'
+    ]
+  }
+
+  let imageBuffer = null
+  try {
+    const image = new Promise((resolve, reject) => {
+      let outBuffer = null
+      const ffmpegProc = spawn(ffmpegBin, args[type])
+
+      ffmpegProc.stdin.on('error', err => {
+        /*
+         * we get an EOF error without this error handler but everything is fine,
+         * log the error if it is anything else
+         */
+        if (err.code === 'EOF') return
+
+        console.error('[FFMPEG stdin Error]', err.message)
+        ffmpegProc.kill()
+      })
+      Readable.from(inputBuffer).pipe(ffmpegProc.stdin)
+
+      // Get output as Buffer
+      const buffers = []
+      ffmpegProc.stdout.on('readable', () => {
+        for (;;) {
+          const buffer = ffmpegProc.stdout.read()
+          if (!buffer) break
+          buffers.push(buffer)
+        }
+      })
+      ffmpegProc.stdout.on('end', () => {
+        outBuffer = Buffer.concat(buffers)
+      })
+
+      ffmpegProc.on('close', () => resolve(outBuffer))
+      ffmpegProc.on('error', err => {
+        console.error('[FFMPEG Error]', err.message)
+        resolve(outBuffer)
+        ffmpegProc.kill()
+      })
+    })
+
+    imageBuffer = await image
+  } catch (err) {
+    imageBuffer = null
+  }
+  return imageBuffer
+}
 
 export default async function generateThumbnail (mimetype, fileBuffer) {
-  if (mimetype.includes('image') === false) return null
+  let imageBuffer
 
-  return sharp(fileBuffer)
+  if (mimetype.includes('image')) {
+    imageBuffer = fileBuffer
+  }
+  if (mimetype.includes('video')) {
+    imageBuffer = await ffmpeg(fileBuffer, 'video')
+  }
+  if (mimetype.includes('audio')) {
+    imageBuffer = await ffmpeg(fileBuffer, 'audio')
+  }
+  if (imageBuffer instanceof Buffer === false) return null
+
+  return sharp(imageBuffer)
   .resize({
     width: 250,
     height: 250,
