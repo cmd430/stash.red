d11b487b60b944e745e4a258036397d781398a65
cmd430 <cmd430@gmail.com>
2023-10-21 02:17:31 +1300

    split logger into seperate file

---
+++ utils/logger.js
@@ -0,0 +1,68 @@
+import { Writable } from 'node:stream'
+import { Log, bold, red, yellow, cyan, green, grey } from 'cmd430-utils'
+
+const fastifyLogger = new Log('Fastify')
+
+export const logger = {
+  level: 'debug',
+  serializers: {
+    res (res) {
+      return {
+        statusCode: res.statusCode,
+        contentLength: res[Object.getOwnPropertySymbols(res).find(s => s.description === 'fastify.reply.headers')]['content-length']
+      }
+    },
+    req (req) {
+      return {
+        method: req.method,
+        url: req.url,
+        path: req.path,
+        parameters: req.parameters
+      }
+    }
+  },
+  stream: new Writable({
+    write (chunk, enc, cb) {
+      //console.debug(chunk.toString())
+
+      const logData = JSON.parse(chunk)
+      const logLevels = {
+        60: 'error',
+        50: 'error',
+        40: 'warn',
+        30: 'info',
+        20: 'debug',
+        10: 'debug'
+      }
+      const status = s => {
+        if (s === '?') return s
+        if (s >= 500) return red(s)
+        if (s >= 400) return yellow(s)
+        if (s >= 300) return cyan(s)
+        if (s >= 200) return green(s)
+        if (s >= 100) return grey(s)
+      }
+      const contentLength = cl => {
+        if (cl === '?') return '?? Bytes'
+
+        const i = Math.floor(Math.log(cl) / Math.log(1024))
+        return `${parseFloat((cl / (1024 ** i)).toFixed((i === 0 ? 0 : 2)))} ${[ 'Bytes', 'KB', 'MB', 'GB' ][i]}`
+      }
+
+      let message = logData.msg
+
+      if (logData.res && !logData.req) {
+        message = `${logData.reqId} - ${bold(status(logData.res.statusCode ?? '?'))} - ${contentLength(logData.res?.contentLength ?? '?')} - ${logData.responseTime?.toFixed(4) ?? 0 ?? '?'}ms`
+      }
+      if (logData.req && !logData.res) {
+        message = `${logData.reqId} - ${bold(grey(logData.req.method))} - ${logData.req.url}${logData.req.path ?? ''}${logData.req.parameters ?? ''}`
+      }
+      if (logData.err) {
+        message = `${logData.reqId} - ${logData.err?.stack}`
+      }
+
+      fastifyLogger[logLevels[logData.level]](message)
+      cb()
+    }
+  })
+}
