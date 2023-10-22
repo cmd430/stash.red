89b9799546d0efc26eec5b231937905ba3efeefa
cmd430 <cmd430@gmail.com>
2023-10-22 16:00:38 +1300

    create album table

---
+++ database/tables/albums.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS "albums" (
+  "_id" INTEGER NOT NULL UNIQUE,
+  "id" TEXT NOT NULL UNIQUE,
+  "title" TEXT NOT NULL,
+  "files" TEXT NOT NULL,
+  "order" TEXT NOT NULL DEFAULT "[]",
+  "uploaded_at" TEXT NOT NULL,
+  "uploaded_by" TEXT NOT NULL DEFAULT "SYSTEM",
+  "ttl" INTEGER,
+  "isPrivate" INTEGER NOT NULL DEFAULT 0,
+  PRIMARY KEY("_id" AUTOINCREMENT)
+);
