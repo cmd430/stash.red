b377a254604394a4f35c51e43b1caead08c9a680
cmd430 <cmd430@gmail.com>
2023-10-24 18:17:11 +1300

    move loadRoutes to plugins

---
+++ plugins/loadRoutes.js
@@ -0,0 +1,33 @@
+import { readdir } from 'node:fs/promises'
+import { Log } from 'cmd430-utils'
+
+// eslint-disable-next-line no-unused-vars
+const { log, debug, info, warn, error } = new Log('Routes')
+
+export default async function loadRoutes (fastify, opts, done) {
+  // Find and register routes
+  for (const file of await readdir('./routes', { recursive: true })) {
+    if (!file.endsWith('.js')) continue
+
+    fastify.register(await import(`../routes/${file}`))
+  }
+
+  // Error handling
+  fastify.setErrorHandler((err, req, reply) => {
+    if (!err.status) return (
+      reply
+        .code(500)
+        .send(err)
+    )
+
+    // TODO: remove stack trace if prod mode
+    err.code = err.status
+
+    reply
+      .code(err.code)
+      .view('error', err)
+  })
+
+  // Move on to other handlers
+  done()
+}
