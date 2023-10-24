323abce30e650da8dea1c830963bae974e09d3a0
cmd430 <cmd430@gmail.com>
2023-10-25 02:11:00 +1300

    load og theme setting from db

--- hooks/fastifiyDefaultLocals.js
+++ hooks/fastifiyDefaultLocals.js
@@ -1,17 +1,23 @@
 import { isDevEnv } from 'cmd430-utils'
+import { config } from '../config/config.js'
+
+const { siteTheme } = config.render
 
 export default function fastifiyDefaultLocals () {
   return Object.values({
     hook: 'preHandler',
     handler (req, reply, done) {
+      const base = req.url.split('?')[0]
+
       reply.locals = {
+        base: `${base}${base.endsWith('/') ? '' : '/'}`,
         title: 'stash.red',
         openGraph: {
           site: req.hostname,
           title: null,
           description: null,
-          theme: isDevEnv() ? '#db0303' : '#3889ea', // TODO: load from config
-          url: `${req.protocol}://${req.hostname}${req.originalUrl}`,
+          theme: isDevEnv() ? siteTheme.dev : siteTheme.prod, // TODO: load from config
+          url: `${req.protocol}://${req.hostname}${req.url}`,
           // The feilds set by the thing are viewing
           image: null,
           video: null,
