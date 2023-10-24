58ee36af1d76e69776294c0b8ae2b952e3d7c64c
cmd430 <cmd430@gmail.com>
2023-10-25 02:20:54 +1300

    username from session, set inAlbum on db and disallow uploads to logged out users

--- routes/post/upload.js
+++ routes/post/upload.js
@@ -1,5 +1,6 @@
 import { customAlphabet } from 'nanoid'
 import { Log } from 'cmd430-utils'
+import createError from 'http-errors'
 import { createAzureBlob, setAzureBlob } from '../../utils/azureBlobStorage.js'
 import generateThumbnail from '../../utils/generateThumbnail.js'
 
@@ -11,28 +12,25 @@ export default function (fastify, opts, done) {
 
   // Upload a file
   fastify.post('/upload', async (req, reply) => {
+    if (!req.session.get('authenticated')) return createError(401)
+
     const files = req.files()
     const fileIDs = []
     const albumID = files.length > 1 ? nanoid(8) : null
+    const username = req.session.get('user').username
 
-    // TODO albumID bound to uploads
     for await (const file of files) {
-
-      // TODO: get username from session
-      const { fileBlobName, azureBlobClients } = createAzureBlob('testAccount', file.filename)
-
+      const { fileBlobName, azureBlobClients } = createAzureBlob(username, file.filename)
       const fileID = nanoid(8)
       const fileBuffer = await file.toBuffer()
       const thumbnailBuffer = await generateThumbnail(file.mimetype, fileBuffer)
       const ttl = parseInt(file.fields?.ttl?.value ?? 0) > 0 ? parseInt(file.fields.ttl.value) : null
-      // TODO: get username from session
-      const username = 'testAccount'
-      // TODO: get isPrivate from upload
+      // TODO: get isPrivate from upload payload
       const isPrivate = Number(false)
 
       fastify.betterSqlite3
-        .prepare('INSERT INTO files (id, name, file, type, uploaded_at, uploaded_by, ttl, isPrivate) VALUES (?, ?, ?, ?, strftime(\'%Y-%m-%dT%H:%M:%fZ\'), ?, ?, ?)')
-        .run(fileID, file.filename, fileBlobName, file.mimetype, username, ttl, isPrivate)
+        .prepare('INSERT INTO files (id, name, file, type, uploaded_at, uploaded_by, ttl, isPrivate, inAlbum) VALUES (?, ?, ?, ?, strftime(\'%Y-%m-%dT%H:%M:%fZ\'), ?, ?, ?, ?)')
+        .run(fileID, file.filename, fileBlobName, file.mimetype, username, ttl, isPrivate, albumID)
 
       await setAzureBlob(fileBuffer, thumbnailBuffer, azureBlobClients)
 
