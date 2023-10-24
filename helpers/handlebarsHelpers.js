2be8dff59cb7debe495ca99390559a54dd0950f4
cmd430 <cmd430@gmail.com>
2023-10-25 02:12:35 +1300

    fix head.hbs so we can parse the local og data and the data given to us

--- helpers/handlebarsHelpers.js
+++ helpers/handlebarsHelpers.js
@@ -1,8 +1,8 @@
 import { extname } from 'node:path'
 import handlebars from 'handlebars'
+import handlebarsPaginate from 'handlebars-paginate'
 
-// TODO: get this working
-//handlebars.registerHelper('paginate', handlebarsPaginate)
+handlebars.registerHelper('paginate', handlebarsPaginate)
 handlebars.registerHelper('if_eq', (a, b, context, opts) => {
   if (context instanceof Function) opts = context
   // eslint-disable-next-line consistent-this, no-invalid-this
@@ -27,26 +27,27 @@ handlebars.registerHelper('if_contains', (a, b, context, opts) => {
 handlebars.registerHelper('split', (data, split, index) => data.split(split)[index])
 handlebars.registerHelper('ext', data => extname(data).slice(1))
 handlebars.registerHelper('json', data => JSON.stringify(data, null, 2))
-handlebars.registerHelper('opengraph', data => {
+handlebars.registerHelper('opengraph', (data, dataExtended) => {
+  const ogData = { ...data, ...dataExtended }
   const meta = []
 
   // Google
-  meta.push(`<meta name="theme-color" content="${data.theme}" />`)
+  meta.push(`<meta name="theme-color" content="${ogData.theme}" />`)
 
   // OpenGraph
-  meta.push(`<meta property="og:site_name" content="${data.site}" />`)
+  meta.push(`<meta property="og:site_name" content="${ogData.site}" />`)
   meta.push('<meta property="og:type" content="website" />')
-  meta.push(`<meta property="og:url" content="${data.url}" />`)
-  if (data.title) meta.push(`<meta property="og:title" content="${data.title}" />`)
-  if (data.description) meta.push(`<meta property="og:description" content="${data.description}" />`)
-  if (data.image) meta.push(`<meta property="og:image" content="${data.image}" />`)
-  if (data.isAlbum === true || data.video || data.audio || data.text) meta.push(`<meta property="og:image" content="${data.url}/thumbnail" />`)
-  if (data.isUser === true) meta.push(`<meta property="og:image" content="${data.avatar}" />`)
+  meta.push(`<meta property="og:url" content="${ogData.url}" />`)
+  if (ogData.title) meta.push(`<meta property="og:title" content="${ogData.title}" />`)
+  if (ogData.description) meta.push(`<meta property="og:description" content="${ogData.description}" />`)
+  if (ogData.image) meta.push(`<meta property="og:image" content="${ogData.image}" />`)
+  if (ogData.isAlbum === true || ogData.video || ogData.audio || ogData.text) meta.push(`<meta property="og:image" content="${ogData.url}/thumbnail" />`)
+  if (ogData.isUser === true) meta.push(`<meta property="og:image" content="${ogData.avatar}" />`)
 
   // Twitter Card
-  if (data.description) meta.push(`<meta property="twitter:description" content="${data.description}" />`)
-  if (data.isAlbum === true && data.isUser !== true && data.image) meta.push('<meta name="twitter:card" content="summary_large_image" />')
-  if (data.isAlbum === true || data.isUser === true || data.video || data.audio || data.text) meta.push('<meta property="twitter:card" content="summary" />')
+  if (ogData.description) meta.push(`<meta property="twitter:description" content="${ogData.description}" />`)
+  if (ogData.isAlbum === true && ogData.isUser !== true && ogData.image) meta.push('<meta name="twitter:card" content="summary_large_image" />')
+  if (ogData.isAlbum === true || ogData.isUser === true || ogData.video || ogData.audio || ogData.text) meta.push('<meta property="twitter:card" content="summary" />')
 
   return meta.join('\n')
 })
