f3ae0cad40c7bafa10219fe70afb3a5eacb65332
cmd430 <cmd430@gmail.com>
2023-10-24 20:49:43 +1300

    openGraph helper updates

--- helpers/handlebarsHelpers.js
+++ helpers/handlebarsHelpers.js
@@ -1,6 +1,7 @@
 import { extname } from 'node:path'
 import handlebars from 'handlebars'
 
+// TODO: get this working
 //handlebars.registerHelper('paginate', handlebarsPaginate)
 handlebars.registerHelper('if_eq', (a, b, context, opts) => {
   if (context instanceof Function) opts = context
@@ -27,19 +28,6 @@ handlebars.registerHelper('split', (data, split, index) => data.split(split)[ind
 handlebars.registerHelper('ext', data => extname(data).slice(1))
 handlebars.registerHelper('json', data => JSON.stringify(data, null, 2))
 handlebars.registerHelper('opengraph', data => {
-
-  console.debug(data)
-
-  data = {
-    theme: 'theme',
-    site: 'site',
-    url: 'url',
-    title: 'title',
-    description: 'description',
-    image: 'image',
-    avatar: 'avatar'
-  }
-
   const meta = []
 
   // Google
@@ -49,16 +37,16 @@ handlebars.registerHelper('opengraph', data => {
   meta.push(`<meta property="og:site_name" content="${data.site}" />`)
   meta.push('<meta property="og:type" content="website" />')
   meta.push(`<meta property="og:url" content="${data.url}" />`)
-  if (data.title !== undefined) meta.push(`<meta property="og:title" content="${data.title}" />`)
-  if (data.description !== undefined) meta.push(`<meta property="og:description" content="${data.description}" />`)
-  if (data.image !== undefined) meta.push(`<meta property="og:image" content="${data.image}" />`)
-  if (data.album !== undefined || data.video !== undefined || data.audio !== undefined || data.text) meta.push(`<meta property="og:image" content="${data.url}/thumbnail" />`)
-  if (data.user !== undefined) meta.push(`<meta property="og:image" content="${data.avatar}" />`)
+  if (data.title) meta.push(`<meta property="og:title" content="${data.title}" />`)
+  if (data.description) meta.push(`<meta property="og:description" content="${data.description}" />`)
+  if (data.image) meta.push(`<meta property="og:image" content="${data.image}" />`)
+  if (data.isAlbum === true || data.video || data.audio || data.text) meta.push(`<meta property="og:image" content="${data.url}/thumbnail" />`)
+  if (data.isUser === true) meta.push(`<meta property="og:image" content="${data.avatar}" />`)
 
   // Twitter Card
-  if (data.description !== undefined) meta.push(`<meta property="twitter:description" content="${data.description}" />`)
-  if (data.album === undefined && data.user === undefined && data.image !== undefined) meta.push('<meta name="twitter:card" content="summary_large_image" />')
-  if (data.album !== undefined || data.video !== undefined || data.audio !== undefined || data.text !== undefined || data.user !== undefined) meta.push('<meta property="twitter:card" content="summary" />')
+  if (data.description) meta.push(`<meta property="twitter:description" content="${data.description}" />`)
+  if (data.isAlbum === true && data.isUser !== true && data.image) meta.push('<meta name="twitter:card" content="summary_large_image" />')
+  if (data.isAlbum === true || data.isUser === true || data.video || data.audio || data.text) meta.push('<meta property="twitter:card" content="summary" />')
 
   return meta.join('\n')
 })
