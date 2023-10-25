ffcdbc0d57a32da5bda3550f6aef695ad954d027
cmd430 <cmd430@gmail.com>
2023-10-25 22:48:15 +1300

    tag multi file uploads with album id

--- routes/post/upload.js
+++ routes/post/upload.js
@@ -13,15 +13,15 @@ export default function (fastify, opts, done) {
   // Upload a file
   fastify.post('/upload', async (req, reply) => {
     if (!req.session.get('authenticated')) return createError(401)
-
     const files = req.files()
     const fileIDs = []
-    const albumID = files.length > 1 ? nanoid(8) : null
+
     const username = req.session.get('user').username
 
     for await (const file of files) {
       const { fileBlobName, azureBlobClients } = createAzureBlob(username, file.filename)
       const fileID = nanoid(8)
+
       const fileBuffer = await file.toBuffer()
       const thumbnailBuffer = await generateThumbnail(file.mimetype, fileBuffer)
       const ttl = parseInt(file.fields?.ttl?.value ?? 0) > 0 ? parseInt(file.fields.ttl.value) : null
@@ -29,8 +29,8 @@ export default function (fastify, opts, done) {
       const isPrivate = Number(false)
 
       fastify.betterSqlite3
-        .prepare('INSERT INTO files (id, name, file, type, uploaded_at, uploaded_by, ttl, isPrivate, inAlbum) VALUES (?, ?, ?, ?, strftime(\'%Y-%m-%dT%H:%M:%fZ\'), ?, ?, ?, ?)')
-        .run(fileID, file.filename, fileBlobName, file.mimetype, username, ttl, isPrivate, albumID)
+        .prepare('INSERT INTO files (id, name, file, type, uploaded_at, uploaded_by, ttl, isPrivate) VALUES (?, ?, ?, ?, strftime(\'%Y-%m-%dT%H:%M:%fZ\'), ?, ?, ?)')
+        .run(fileID, file.filename, fileBlobName, file.mimetype, username, ttl, isPrivate)
 
       await setAzureBlob(fileBuffer, thumbnailBuffer, azureBlobClients)
 
@@ -38,14 +38,25 @@ export default function (fastify, opts, done) {
       fileIDs.push(fileID)
     }
 
-    debug({
-      albumID,
-      fileIDs
-    })
+    debug({ fileIDs })
 
-    if (fileIDs.length === 1) return reply.redirect(`/f/${fileIDs[0]}`)
+    // Generate Album if muliple files
+    if (fileIDs.length > 1) {
+      const albumID = nanoid(8)
 
-    return reply.redirect(`/a/${albumID}`)
+      debug({ albumID })
+
+      const statement = fastify.betterSqlite3.prepare('UPDATE files SET inAlbum = ? WHERE id = ?')
+      const transaction = fastify.betterSqlite3.transaction((fIDs, aID) => fIDs.map(fID => statement.run(aID, fID)))
+      const updated = transaction(fileIDs, albumID)
+        .reduce((accumulator, currentValue) => (accumulator += currentValue.changes), 0)
+
+      debug('Added', updated, 'files to album')
+
+      return reply.redirect(`/a/${albumID}`)
+    }
+
+    return reply.redirect(`/f/${fileIDs[0]}`)
   })
 
   done()
