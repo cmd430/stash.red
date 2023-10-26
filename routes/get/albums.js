f20e021c1318ada3afcb78bee7dc4cc2c5a47071
cmd430 <cmd430@gmail.com>
2023-10-27 00:44:07 +1300

    we can now see album thumbnails on the user page

--- routes/get/albums.js
+++ routes/get/albums.js
@@ -1,10 +1,10 @@
+import createError from 'http-errors'
 import { Log } from 'cmd430-utils'
+import { getAzureBlobBuffer } from '../../utils/azureBlobStorage.js'
 
 // eslint-disable-next-line no-unused-vars
 const { log, debug, info, warn, error } = new Log('Albums (GET)')
 
-// TODO: Add 404 etc error handling
-// TODO: Respect isPrivate Flags
 // TODO: Album pages
 
 export default function (fastify, opts, done) {
@@ -12,39 +12,37 @@ export default function (fastify, opts, done) {
   // Get album by ID
   fastify.get('/a/:id', async (req, reply) => {
     const { id } = req.params
-
-    const { title, order, isPrivate } = fastify.betterSqlite3
-      .prepare('SELECT title, `order`, isPrivate FROM albums WHERE id = ?')
+    const dbResult = fastify.betterSqlite3
+      .prepare('SELECT uploaded_at, uploaded_by, title, files, total, isPrivate FROM album WHERE id = ?')
       .get(id)
+
+    if (!dbResult) return createError(404)
+
+    const { uploaded_at, uploaded_by, title, files, total, isPrivate } = dbResult
+
+    debug({ uploaded_at, uploaded_by, title, files, total, isPrivate })
 
     return {
       message: 'WIP',
       album: id,
-      data: {
-        title, order, isPrivate
-      }
+      data: dbResult
     }
   })
 
   // Get album thumbnail
   fastify.get('/a/:id/thumbnail', async (req, reply) => {
     const { id } = req.params
-
-    // TODO: Get thumbnail of first file in album
-
-    return {
-      message: 'WIP',
-      album: id
-    }
-  })
-
-  // Get info for album
-  fastify.get('/a/:id/info', async (req, reply) => {
-    const { id } = req.params
-
-    return fastify.betterSqlite3
-      .prepare('SELECT * FROM albums WHERE id = ?')
+    const dbResult = fastify.betterSqlite3
+      .prepare('SELECT thumbnail, uploaded_by FROM album WHERE id = ?')
       .get(id)
+
+    if (!dbResult) return createError(404)
+
+    const { thumbnail, uploaded_by } = dbResult
+
+    return reply
+      .type('image/webp')
+      .send(await getAzureBlobBuffer(uploaded_by, thumbnail))
   })
 
   done()
