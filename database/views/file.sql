081ad8b0a0d9426c2d08446ca6f7dcddec03c835
cmd430 <cmd430@gmail.com>
2023-10-27 01:29:31 +1300

    format sql files better

--- database/views/file.sql
+++ database/views/file.sql
@@ -1,4 +1,2 @@
-DROP VIEW "main"."file";
-CREATE VIEW "file" AS
-SELECT id, name, file, type, uploaded_at, uploaded_by, isPrivate, 'thumbnail/thumbnail_'||replace(file, ltrim(file, replace(file, '.', '' ) ), '')||'.webp' AS thumbnail
-FROM files
+CREATE VIEW IF NOT EXISTS "file" AS
+SELECT id, name, file, type, uploaded_at, uploaded_by, isPrivate, 'thumbnail/thumbnail_'||replace(file, ltrim(file, replace(file, '.', '' ) ), '')||'.webp' AS thumbnail FROM files;
