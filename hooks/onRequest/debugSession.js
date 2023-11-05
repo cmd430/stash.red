777c05bc46768d2162cd35798bb139ce7a2c24b5
cmd430 <cmd430@gmail.com>
2023-11-06 02:33:51 +1300

    add session debug logging as onRequest hook

---
+++ hooks/onRequest/debugSession.js
@@ -0,0 +1,14 @@
+import { Log } from 'cmd430-utils'
+
+// eslint-disable-next-line no-unused-vars
+const { log, debug, info, warn, error } = new Log('Session Debug')
+
+export default function handler (request, reply, done) {
+
+  debug({
+    authenticated: (request.session.get('authenticated') ?? false),
+    ...request.session.get('session')
+  })
+
+  done()
+}
