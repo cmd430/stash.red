0a53a2c66f108dfe4a7681a4ee97a08aeb21c8ba
cmd430 <cmd430@gmail.com>
2023-11-06 02:26:02 +1300

    move route error handling into its own file

--- plugins/loadRoutes.js
+++ plugins/loadRoutes.js
@@ -2,35 +2,10 @@ import { readdir } from 'node:fs/promises'
 import { Log } from 'cmd430-utils'
 import createError from 'http-errors'
 import { fastifyPlugin } from 'fastify-plugin'
+import errorHandler from '../utils/errorHandler.js'
 
 // eslint-disable-next-line no-unused-vars
 const { log, debug, info, warn, error } = new Log('Routes')
-
-function handleError (err, request, reply) {
-  if (!err.status && err.statusCode) err.status = err.statusCode
-  if (!err.status) {
-    err.status = 500
-    err.message = 'Internal Server Error'
-    error(err.stack)
-  }
-
-  const errorDescriptions = {
-    '400': 'The request cannot be fulfilled',
-    '401': 'You do not have permission for this',
-    '403': 'You are forbidden to from this',
-    '404': 'The requested page could not be found',
-    '500': 'Something has gone wrong processing the request'
-  }
-
-  reply
-    .code(err.status)
-    .view('error', {
-      status: err.status,
-      message: err.message,
-      description: err.description ?? errorDescriptions[err.status] ?? 'An unknown error has occurred',
-      stack: err.stack
-    })
-}
 
 export default fastifyPlugin(async (fastify, opts, done) => {
   // Find and register routes
@@ -41,8 +16,8 @@ export default fastifyPlugin(async (fastify, opts, done) => {
   }
 
   // Error handling
-  fastify.setNotFoundHandler((request, reply) => handleError(createError(404), request, reply))
-  fastify.setErrorHandler((err, request, reply) => handleError(err, request, reply))
+  fastify.setNotFoundHandler((request, reply) => errorHandler(createError(404), request, reply))
+  fastify.setErrorHandler((err, request, reply) => errorHandler(err, request, reply))
 
   // Move on to other handlers
   done()
