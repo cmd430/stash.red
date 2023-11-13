31ed3930fc8d07d266a52538f41633187dd77f40
Bradley Treweek <cmd430@gmail.com>
2023-11-13 23:16:29 +1300

    update sessionGC to be consistant with tempUploadsGC

--- plugins/sessionGC.js
+++ plugins/sessionGC.js
@@ -5,8 +5,8 @@ import { fastifyPlugin } from 'fastify-plugin'
 // eslint-disable-next-line no-unused-vars
 const { log, debug, info, warn, error } = new Log('Session GC')
 
-async function performGC (db) {
-  const { changes: removed } = db
+async function performGC (fastify) {
+  const { changes: removed } = fastify.betterSqlite3
     .prepare('DELETE FROM "session" WHERE "expires" < strftime(\'%Y-%m-%dT%H:%M:%fZ\', \'now\')')
     .run()
 
@@ -16,8 +16,8 @@ async function performGC (db) {
 export default fastifyPlugin((fastify, opts, done) => {
   const { session: { gcInterval } } = fastify.config
 
-  performGC(fastify.betterSqlite3)
-  setInterval(() => performGC(fastify.betterSqlite3), evaluate(gcInterval))
+  performGC(fastify)
+  setInterval(() => performGC(fastify), evaluate(gcInterval))
   done()
 }, {
   fastify: '4.x',
