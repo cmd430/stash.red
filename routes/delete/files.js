095c3e9661e66b70b64ff0f22243cf69517a8972
cmd430 <cmd430@gmail.com>
2023-11-17 02:19:05 +1300

    fix for new return

--- routes/delete/files.js
+++ routes/delete/files.js
@@ -12,9 +12,9 @@ export default function (fastify, opts, done) {
     if ((request.session.get('authenticated') ?? false) === false) return createError(401) // Not authd
 
     const { username } = request.session.get('session') ?? null
-    const { succeeded, data, reason } = await fastify.db.deleteFile(id, username)
+    const { succeeded, code, data } = await fastify.db.deleteFile(id, username)
 
-    if (succeeded === false) return createError(reason.code)
+    if (succeeded === false) return createError(code)
 
     await fastify.storage.delete(username, data.file)
 
