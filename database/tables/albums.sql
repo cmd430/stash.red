b0523dbd5dcebfa1463e5bc415d40d2575b760a1
cmd430 <cmd430@gmail.com>
2023-11-18 22:35:37 +1300

    db schema adjustments

--- database/tables/albums.sql
+++ database/tables/albums.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS "albums" (
   "title" TEXT NOT NULL DEFAULT "Untitled Album",
   "uploadedBy" TEXT NOT NULL DEFAULT "SYSTEM",
   "uploadedAt" DATETIME NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ')),
-  "ttl" INTEGER,
-  "isPrivate" INTEGER NOT NULL DEFAULT 0,
+  "uploadedUntil" DATETIME NOT NULL DEFAULT Infinity,
+  "isPrivate" BOOLEAN NOT NULL DEFAULT FALSE,
   PRIMARY KEY("id"),
   FOREIGN KEY("uploadedBy") REFERENCES "accounts"("username") ON UPDATE CASCADE ON DELETE CASCADE
 );
