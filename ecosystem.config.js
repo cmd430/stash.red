33a9e00a4c890aa5df4636c47de845fb852a7bab
cmd430 <cmd430@gmail.com>
2023-11-19 23:10:48 +1300

    add pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+// PM2 Ecosystem file
+// https://pm2.keymetrics.io/docs/usage/application-declaration/#generate-configuration
+
+module.exports = {
+  apps: [ {
+    name: 'stash',
+    script: 'main.js',
+    watch: false,
+    'args': [
+      '--color'
+    ],
+    error_file: 'logs/err.log',
+    out_file: 'logs/out.log',
+    log_file: 'logs/combined.log',
+    time: false
+  } ]
+}
