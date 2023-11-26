276d376d163a455ec6adb05c08a9b0771888735c
cmd430 <cmd430@gmail.com>
2023-11-26 22:39:23 +1300

    remove commented out code

--- interfaces/database/sqliteDatabase.js
+++ interfaces/database/sqliteDatabase.js
@@ -37,9 +37,6 @@ export default class DatabaseInterface extends DatabaseInterfaceBase {
       encoding: 'utf8'
     }))
 
-    // Optimize DB every ~24h
-    // setInterval(() => this.#database.pragma('optimize'), 1000 * 60 * 60 * 24)
-
     // Gracefully close the DB on exit
     process.on('exit', () => {
       this.#database.close()
