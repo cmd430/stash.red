50b9cb17fd4cae07ffd7d156e0d3fdfdd3878ec6
cmd430 <cmd430@gmail.com>
2023-12-08 18:58:30 +1300

    add line numbers to text uploads

--- public/js/partials/text.js
+++ public/js/partials/text.js
@@ -1,10 +1,13 @@
 const textFiles = document.querySelectorAll('.wrapper[data-type="text"]')
 
 for (const textFile of textFiles) {
-  const code = textFile.querySelector('pre.text > code')
+  const text = textFile.querySelector('pre.text')
 
-  fetch(code.textContent)
+  fetch(text.textContent)
     .then(r => r.text())
-    .then(text => (code.textContent = text))
+    .then(content => {
+      text.textContent = null
+      text.insertAdjacentHTML('beforeend', `<span class="line">${content.split(/\n/).join('\n</span><span class="line">')}</span>`)
+    })
     .catch(err => console.error(err))
 }
